8473cc938bac976487a9aa75e1fb7e5e69e933e3
gudwin <nrg.gudwin@gmail.com>
2017-03-04 11:47:23 +0200

    rename

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import pytest
 
 @pytest.fixture(scope="module")
-def driver():
+def driver(request):
     driver = webdriver.Firefox()
     yield driver
     driver.quit()
