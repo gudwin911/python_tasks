f169d01e6311552289fee87bee552e5a0a51ee53
gudwin <nrg.gudwin@gmail.com>
2017-04-03 17:04:41 +0300

    effort2

--- trying_make_Page_Object.py
+++ trying_make_Page_Object.py
@@ -4,17 +4,18 @@ import pytest
 class HomePage():
 
     def __init__(self, driver):
+        self.driver = driver
         self.driver.get("https://jysk.ua")
 
-    def get_search_field(self, driver):
+    def get_search_field(self):
         inputField = self.driver.find_element_by_name("query")
         return inputField
 
     def input_text_in_search_field(self):
-        self.inputField.send_keys("RYSLINGE")
+        self.get_search_field().send_keys("RYSLINGE")
 
     def submit_data_from_search_field(self):
-        self.inputField.submit()
+        self.input_text_in_search_field().submit()
 
 
     # def test_jusk7(self, driver): # залышыты видгук...мем надислаты видгук, форма не закрылась и что чекбокс подсвечен красным
