c13bb84599773e2c01ad46318ce2ce18d43c97c3
gudwin <nrg.gudwin@gmail.com>
2017-04-20 15:27:09 +0300

    case to check

--- Page.py
+++ Page.py
@@ -1,8 +1,4 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import pytest
 
 
 class BasePage(object):
@@ -10,15 +6,15 @@ class BasePage(object):
         self.driver = driver
         self.driver.implicitly_wait(10)
 
-    # def get_search_field(self):
-    #     return self.driver.find_element(By.NAME, "query")
-
     def scroll_down(self, num):
         self.driver.execute_script("window.scrollTo(0, %s);" % num)
 
     def search(self, param1):
-        self.driver.find_element(By.NAME, "query").send_keys(param1).submit()
+        txt = self.driver.find_element(By.NAME, "query")
+        txt.send_keys(param1)
+        txt.submit()
         return SearchResultPage(self.driver)
+
 
 class HomePage(BasePage):
     def __init__(self, driver):
@@ -26,87 +22,14 @@ class HomePage(BasePage):
         self.driver.get("https://jysk.ua")
         self.driver.implicitly_wait(10)
 
+
 class SearchResultPage(BasePage):
-
-    # def wait_search_result(self):
-    #     WebDriverWait(self.driver, 10).until(
-    #         EC.visibility_of_element_located((By.CLASS_NAME, "view-header")))
-
     def to_product(self, param1):
+        self.scroll_down(400)
         self.driver.find_element(By.XPATH, param1).click()
         return ProductPage(self.driver)
 
-    # def get_products(self):
-    #     return self.driver.find_elements(By.CLASS_NAME, "product")
-    #
-    # def get_product_img(self):
-    #      return self.driver.find_element(By.XPATH, "//div[@class='col-ms-6']//img[1]")
 
 class ProductPage(BasePage):
     def product_name(self):
         return self.driver.find_element(By.CLASS_NAME, "widgets-enabled").text
-
-    # def get_product_specs(self):
-    #     return self.driver.find_element(By.CLASS_NAME, "product-specs").text
-
-
-    # def wait_for_content_download(self):
-    #     WebDriverWait(self.driver, 10).until(
-    #         EC.visibility_of_element_located((By.XPATH, "//*[@id='content-tab-bar']/li[2]/a")))
-    #
-    # def get_reviews_button(self):
-    #     return self.driver.find_element(By.XPATH, "//*[@id='content-tab-bar']/li[2]/a")
-    #
-    # def wait_for_reviews_tab_visibility(self):
-    #     WebDriverWait(self.driver, 10).until(
-    #         EC.visibility_of_element_located((By.ID, "product-ratings")))
-    #
-    # def get_new_review_button(self):
-    #     return self.driver.find_element(By.ID, "product-ratings")
-
-
-# class ReviewPage(BasePage):
-#
-#     def wait_for_content_download(self):
-#         WebDriverWait(self.driver, 10).until(
-#             EC.presence_of_element_located((By.ID, "edit-title")))
-#
-#     def get_select_rating_button(self):
-#         return self.driver.find_element(By.XPATH, "//*[@id='rating-select']/div/span[5]")
-#
-#     def get_review_title(self):
-#         return self.driver.find_element_by_id("edit-title")
-#
-#     def get_review_text(self):
-#         return self.driver.find_element_by_id("edit-body")
-#
-#     def get_review_author(self):
-#         return self.driver.find_element_by_id("edit-author")
-#
-#     def get_review_age_dropbox(self):
-#         return self.driver.find_element_by_id("edit-age")
-#
-#     def get_review_age_option(self):
-#         return self.driver.find_element_by_xpath("//*[@id='edit-age']/option[4]")
-#
-#     def get_review_sex_dropbox(self):
-#         return self.driver.find_element_by_id("edit-sex")
-#
-#     def get_review_sex_option(self):
-#         return self.driver.find_element_by_xpath("//*[@id='edit-sex']/option[2]")
-#
-#     def get_review_city(self):
-#         return self.driver.find_element_by_id("edit-city")
-#
-#     def get_review_email(self):
-#         return self.driver.find_element_by_id("edit-email")
-#
-#     def get_review_submit(self):
-#         return self.driver.find_element_by_id("edit-submit--4")
-#
-#     def wait_validation_error(self):
-#         WebDriverWait(self.driver, 10).until(
-#             EC.visibility_of_element_located((By.CLASS_NAME, "validation-failed")))
-#
-#     def get_personal_data_checkbox(self):
-#         return self.driver.find_element(By.XPATH, "//*[@id='jysk-reviews-add-review-form']/div/div/div[9]")
--- try.py
+++ try.py
@@ -1,22 +1,13 @@
 from python_tasks import Page
 import pytest
 
-# class s():
-#     def testy(self):
-#         page = Page.HomePage
-#         page.get_search_field().send_keys("RYSLINGE").submit()
-#         page = Page.SearchResultPage
-#         page.wait_search_result()
-#         page.scroll_down(400)
-#         page.get_product_img().click()
-#         page = Page.ProductPage
-#         page.wait_for_content_download()
-#         txt = page.get_product_name()
-#         assert txt == "СТІЛ RYSLINGE + 4 СТІЛЬЦІ RYSLINGE"
 
-class Test(pytest):
-    def test_check_product_name(self):
-        home = Page.HomePage(self.driver)
+class Test():
+    def test_check_product_name(self, driver):
+        home = Page.HomePage(driver)
         result = home.search("RYSLINGE")
-        product_page = result.to_product("//div[@class='col-ms-6']//img[1]")
-        assert "СТІЛ RYSLINGE + 4 СТІЛЬЦІ RYSLINGE" in product_page.product_name()
+        product_page = result.to_product("//img[@alt='Стіл RYSLINGE + 4 стільці RYSLINGE']")
+        assert "Стіл RYSLINGE + 4 стільці RYSLINGE" in product_page.product_name()
+
+if __name__ == "__main__":
+    pytest.main()
