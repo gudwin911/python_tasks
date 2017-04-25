c5126c01fb1c19a7b57384af22e2e386883f46a9
vgalag <nrg.gudwin@gmail.com>
2017-04-25 11:52:12 +0300

    first var

--- Page.py
+++ Page.py
@@ -43,16 +43,72 @@ class ProductPage(BasePage):
         return self.driver.find_element(By.CLASS_NAME, "widgets-enabled").text
 
     def product_description(self):
-        return self.driver.find_element(By.CSS_SELECTOR, "span[itemprop='description']").text
+        return self.driver.find_element(By.CSS_SELECTOR, "span[@itemprop='description']").text
 
     def header_review_quantity(self):
-        return self.driver.find_element_by_class_name("review-count").text
+        return self.driver.find_element(By.CLASS_NAME, "review-count").text
 
     def open_reviews(self):
         self.scroll_down(900)
-        self.driver.find_element_by_xpath("//*[@id='content-tab-bar']/li[2]/a").click()
+        self.driver.find_element(By.XPATH, "//*[@id='content-tab-bar']/li[2]/a").click()
 
     def reviews_count(self):
-        self.scroll_down(900)
-        self.driver.find_element_by_xpath("//*[@id='content-tab-bar']/li[2]/a").click()
-        return len(self.driver.find_elements_by_class_name("field-content"))
+        return len(self.driver.find_elements(By.CLASS_NAME, "field-content"))
+
+    def btn_write_review(self):
+        self.driver.find_element(By.XPATH, "//a[@href='#notification']").click()
+        return ReviewPage(self.driver)
+
+
+class ReviewPage(BasePage):
+
+    def select_rating(self, num):
+        self.driver.find_element(By.XPATH, "//*[@id='rating-select']/div/span[%s]" % num).click()
+
+    def add_title(self, txt):
+        self.driver.find_element(By.ID, "edit-title").send_keys(txt)
+
+    def add_comment(self, txt):
+        self.driver.find_element(By.ID, "edit-body").send_keys(txt)
+
+    def add_author(self, name):
+        self.driver.find_element(By.ID, "edit-author").send_keys(name)
+
+    def select_age(self, option):
+        self.driver.find_element(By.ID, "edit-age").click()
+        self.driver.find_element(By.XPATH, "//*[@id='edit-age']/option[%s]" % option).click()
+
+    def select_sex(self, sex):
+        self.driver.find_element(By.ID, "edit-sex").click()
+        self.driver.find_element(By.XPATH, "//*[@id='edit-sex']/option[@value='%s']" % sex).click()
+
+    def add_city(self, city):
+        self.driver.find_element(By.ID, "edit-city").send_keys(city)
+
+    def add_mail(self, mail):
+        self.driver.find_element(By.ID, "edit-email").send_keys(mail)
+
+    def edit_accept_terms(self, bool):
+        if bool:
+            self.driver.find_element(By.ID, "edit-accept-terms").click()
+
+    def send_review(self):
+        self.driver.find_element(By.ID, "edit-submit").submit()
+
+    def add_review(self, rating, title, txt, author, age, sex, city, mail, box):
+        self.select_rating(rating).\
+            add_title(title).\
+            add_comment(txt).\
+            add_author(author).\
+            select_age(age).\
+            select_sex(sex).\
+            add_city(city).\
+            add_mail(mail).\
+            edit_accept_terms(box).\
+            send_review()
+
+    def get_error_msg_class(self):
+        WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "validation-failed")))
+        return self.driver.find_element(By.XPATH, "//*[@id='jysk-reviews-add-review-form']/div/div/div[9]").\
+            get_attribute("class")
--- try.py
+++ try.py
@@ -5,20 +5,30 @@ import pytest
 class Test():
     name = "Стіл RYSLINGE + 4 стільці RYSLINGE"
     query = "RYSLINGE"
+    # review data
+    rating = 4
+    title = "Title"
+    txt = "Some text"
+    author = "Greber"
+    age = 3
+    sex = "male"
+    city = "London"
+    mail = "chekount@gmail.com"
+    box = False
 
     def product_page(self, driver):
         return Page.HomePage(driver).\
                 search(self.query).\
                 to_product(self.name)
 
-    # проверяем что количество товаров по запросу равно 8
+    # 1 проверяем что количество товаров по запросу равно 8
     def test_check_product_quantity(self, driver):
         product_quantity = Page.HomePage(driver).\
             search(self.query).\
             count_products(self.name)
         assert product_quantity == 8
 
-    # проверяем что вверху верно отображено название
+    # 2 проверяем что вверху верно отображено название
     def test_check_product_name(self, driver):
         product_name = Page.HomePage(driver).\
             search(self.query).\
@@ -26,7 +36,7 @@ class Test():
             product_name()
         assert self.name in product_name
 
-    # проверяем что в описании есть текст “В комплект входять 2 додаткових 'крила'"
+    # 3 проверяем что в описании есть текст “В комплект входять 2 додаткових 'крила'"
     def test_check_product_description(self, driver):
         description = Page.HomePage(driver).\
             search(self.query).\
@@ -34,7 +44,7 @@ class Test():
             product_description()
         assert "В комплект входять 2 додаткових 'крила'" in description
 
-    # проверяем, что есть 4 отзыва вверху
+    # 4 проверяем, что есть 4 отзыва вверху
     def test_check_reviews_quantity_up_page(self, driver):
         quantity = Page.HomePage(driver).\
             search(self.query).\
@@ -42,16 +52,22 @@ class Test():
             header_review_quantity()
         assert quantity == "4 відгуків"
 
-    # проверяем, что есть 4 отзыва, если нажать на видугки внизу
+    # 5 проверяем, что есть 4 отзыва, если нажать на видугки внизу
     def test_check_reviews_counter(self, driver):
         quantity = self.product_page(driver).\
+            open_reviews().\
             reviews_count()
         assert quantity == 4
 
-    # залышыты видгук...мем надислаты видгук, форма не закрылась и что чекбокс подсвечен красным
+    # 6 залышыты видгук...мем надислаты видгук, форма не закрылась и что чекбокс подсвечен красным
     def test_check_review_checkbox_error_message(self, driver):
         error = self.product_page(driver).\
-            open_reviews()
+            open_reviews().\
+            btn_write_review().\
+            add_review(self.rating, self.title, self.txt, self.author,
+                       self.age, self.sex, self.city, self.mail, self.box).\
+            get_error_msg_class()
+        assert "not-validated" in error
 
 
 if __name__ == "__main__":
